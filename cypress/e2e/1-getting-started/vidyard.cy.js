4a5e235787ec781555725ca3b9a3c10b0f0cc3f3
j-maddo <105676644+j-maddo@users.noreply.github.com>
2022-11-30 10:28:23 -0500

    Update vidyard.cy.js
    remove cred

--- cypress/e2e/1-getting-started/vidyard.cy.js
+++ cypress/e2e/1-getting-started/vidyard.cy.js
@@ -4,9 +4,6 @@ context('Cypress', function () {
       // Please sign up (for free) at www.vidyard.com, verify your email address, then update these accordingly.
       // This test also requires at least one video in your Library, so please upload at least one video.
   
-      const email = 'jireh@cypress.io';
-      const password = 'Faithful08!';
-  
       cy.visit('https://secure.vidyard.com/user/sign_in');
       cy.get('#username').type(email);
       cy.get('#next-login').click();
